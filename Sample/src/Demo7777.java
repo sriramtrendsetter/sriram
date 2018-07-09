e2cff96db24f563570a0c483408a0914fe9e578d
pallamreddyanudeep <40880859+pallamreddyanudeep@users.noreply.github.com>
2018-07-09 18:56:43 +0530

    2nd edited

--- Sample/src/Demo7777.java
+++ Sample/src/Demo7777.java
@@ -4,6 +4,7 @@ public class Demo7777 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("in second class");
+		System.out.println("happy");
 	}
 
 }
